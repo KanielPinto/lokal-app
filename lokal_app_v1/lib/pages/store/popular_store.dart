e5e1daa08e5b9605b8482961c3cfee7fe912196f
KanielPinto <kaniel.pinto@xaviers.edu.in>
2023-03-02 18:23:08 +0530

    UI checkpoint

--- lokal_app_v1/lib/pages/store/popular_store.dart
+++ lokal_app_v1/lib/pages/store/popular_store.dart
@@ -6,6 +6,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:lokal_app_v1/utils/colors.dart';
 import 'package:lokal_app_v1/utils/dimensions.dart';
 import 'package:lokal_app_v1/widgets/app_icon.dart';
+import 'package:lokal_app_v1/widgets/expandable_text.dart';
 import 'package:lokal_app_v1/widgets/header_text.dart';
 import 'package:lokal_app_v1/widgets/iconplustext.dart';
 import 'package:lokal_app_v1/widgets/infoDrawer.dart';
@@ -20,6 +21,7 @@ class PopularStore extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
+          //background image
           Positioned(
               left: 0,
               right: 0,
@@ -31,6 +33,7 @@ class PopularStore extends StatelessWidget {
                         fit: BoxFit.cover,
                         image: AssetImage("assets/image/store0.jpg"))),
               )),
+          //Store Blip
           Positioned(
               top: Dimensions.height45,
               left: Dimensions.width20,
@@ -42,6 +45,7 @@ class PopularStore extends StatelessWidget {
                   AppIcon(icon: Icons.shopping_cart_outlined),
                 ],
               )),
+          //Store Info
           Positioned(
             left: 0,
             right: 0,
@@ -68,6 +72,16 @@ class PopularStore extends StatelessWidget {
                     height: Dimensions.height20,
                   ),
                   HeaderText(text: "Store Info"),
+                  SizedBox(
+                    height: Dimensions.height10,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: ExpandableText(
+                          text:
+                              " Sparkle & Shine Bracelets is a small business that was founded with the intention of creating unique and stylish bracelets that make women feel confident and empowered. Jane, the founder of Sparkle & Shine Bracelets, is a passionate jewelry maker who has always had a love for designing beautiful pieces. \n\n Jane's journey as a jewelry maker began when she was young, making friendship bracelets for her friends and family. As she grew older, her passion for creating jewelry grew as well, and she began experimenting with different materials and techniques to perfect her craft. After years of honing her skills, she decided to turn her hobby into a business, and Sparkle & Shine Bracelets was born. \n\n Every bracelet that Sparkle & Shine Bracelets creates is carefully crafted with attention to detail, using only the highest quality materials. Jane sources her materials from all over the world, including semi-precious stones, beads, and charms. She ensures that every piece she creates is not only beautiful but also durable and long-lasting. \n\n Sparkle & Shine Bracelets offers a wide range of styles, from elegant and sophisticated to bohemian and casual. Jane designs each bracelet with the intention of creating a unique piece that will complement any outfit and make the wearer feel confident and stylish. Each piece is handmade with love, care, and attention to detail, ensuring that no two bracelets are alike. \n\n Jane's mission is to create beautiful, affordable bracelets that make women feel confident and empowered. She believes that a great piece of jewelry can make all the difference in a person's day, and her goal is to provide her customers with pieces that they will love and cherish for years to come. \n\n Whether you're looking for a special gift for someone you love or just want to treat yourself to something special, Sparkle & Shine Bracelets has something for everyone. From classic and timeless styles to trendy and modern pieces, there's a bracelet to suit every taste and occasion. With Sparkle & Shine Bracelets, you can be confident that you're getting a unique, high-quality piece of jewelry that will bring a touch of sparkle and shine to your life"),
+                    ),
+                  )
                 ],
               ),
             ),
--- lokal_app_v1/lib/utils/dimensions.dart
+++ lokal_app_v1/lib/utils/dimensions.dart
@@ -24,6 +24,8 @@ class Dimensions {
   static double width45 = screenHeight / 18.76;
 
   //font size
+  static double font16 = screenHeight / 52.75;
+
   static double font26 = screenHeight / 32.46;
   static double font20 = screenHeight / 42.2;
 
